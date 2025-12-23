4ce6b07230d30945e08478fbe1c50652e35bb46e
Karl Naranjo <knaranjovaldebenito@gmail.com>
2025-12-23 10:35:08 -0300

    el mundial

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>EL RAYO MACQUIN</title>
+    <title>EL RAYO MACQUIN Y EL GOLAZO DE RIQUELME</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Google Font Vaporwave -->
